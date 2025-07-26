be772a185823835b9333114a2ad147be005d2882
Evolone Layne <elayne297@gmail.com>
2025-07-26 10:13:01 -0700

    hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import './index.css';
 import nasaLogo from './assets/nasa-logo.png';
 import marsImage from './assets/mars.webp';
